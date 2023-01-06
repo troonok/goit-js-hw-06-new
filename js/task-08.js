c35d7f1ba32c2595382aca189121130466410e03
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-01-07 00:53:43 +0200

    добавлен метод trim для корректной проверки заполнения поля

--- js/task-08.js
+++ js/task-08.js
@@ -8,7 +8,7 @@ function handleSubmit(event) {
       elements: { email, password }
     } = event.currentTarget;
   
-    if (email.value === "" || password.value === "") {
+    if (email.value.trim() === "" || password.value === "") {
       return alert("Please fill in all the fields!");
     }
   
